be7a76d8ea633311e8efc487b4cfaabc06b5a5ad
Daniel Steinbrook <danielsteinbrook@Daniels-MacBook-Pro.local>
2022-08-13 19:09:10 -0400

    jobs display test

--- src/client/components/JobDisplay.jsx
+++ src/client/components/JobDisplay.jsx
@@ -1,9 +1,41 @@
 import React from 'react';
 
+const test = {
+    jobTitle: "Sr. Software Engineer",
+    companyName: "Google",
+    jobListingUrl: "http://www.google.com",
+    dateLastChecked: "08-08-08",
+
+}
+
 const JobDisplay = (props) => {
     return (
         <div className= "jobDisplay">
-            JobDisplayTest
+            <div className= "jobTitle">
+               {test.jobTitle}
+            </div>
+            <div className= "jobCompanyName">
+               {test.companyName}
+            </div>
+            <div className= "jobListingUrl">
+              <a href = {test.jobListingUrl}>Job Listing</a>
+            </div>
+            <div className= "jobDateLastChecked">
+               {test.dateLastChecked}
+            </div>
+            <label for="status">Status</label>
+            <select name="status" id="status">
+                <option value="interested">Interested</option>
+                <option value="applied">Applied</option>
+                <option value="interviewing">Interviewing</option>
+                <option value="declined">Declined</option>
+            </select>
+            <div id="notes">
+               Notes
+               <div contenteditable="true" id = "notesField">
+
+               </div>
+            </div>
 
         </div>
     )
