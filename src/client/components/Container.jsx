c68693ef5a718605bc44f97e04adcdc55c27fa6c
Daniel Steinbrook <danielsteinbrook@Daniels-MacBook-Pro.local>
2022-08-13 17:31:47 -0400

    tabsComponent

--- src/client/components/Container.jsx
+++ src/client/components/Container.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import TabsContainer from "./TabsContainer";
+import JobList from "./JobList";
+import CompanyList from "./CompanyList"
 
 const Container = (props) => {
   return (
-   <div className="container">
-    containerTest
+   <div>
+    <h1>Tabs Test</h1>
       
-     <TabsContainer />
+     <TabsContainer>
+       <div label = "Jobs">
+        <JobList />
+       </div>
+       <div label = "Companies">
+        <CompanyList />
+       </div>
+       </TabsContainer>
+       
+
+     
     
     </div>);
 }
