56d2998e4e272af4a76af46cac207345b90ff1e0
jakejurado <97064241+jakejurado@users.noreply.github.com>
2022-08-15 08:51:58 -0700

    jwt token is set up to create and verify

--- src/server/index.js
+++ src/server/index.js
@@ -3,27 +3,44 @@ const path = require('path');
 const pg = require('pg');
 const cookieParser = require('cookie-parser');
 const port = 3000;
+const app = express();
 const loginControllers = require('./routes/loginControllers');
 
-const app = express();
+//jwt
+const dotenv = require('dotenv').config('../.env');
+const jwt = require('jsonwebtoken');
+//const JWT_SECRET = '0fjkkldfj838y8ryy92oihjfdjfhajkf2DKFEHIOjkdfAj5792jDfjkdfdDfldfkjajfkIEjodkfjkkbvnzadkjfatdjfkVNEujfjdk4810df0f9fd034Jdo34jlr3fd'
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.static(path.resolve(__dirname, '../dist')));
 
 
 
+
+
+
+
 app.get('/api', (req, res) => {
-  console.log(name);
+  console.log('hi');
 });
 
 app.post('/signup', loginControllers.createUser, (req, res) => {
-  return res.status(200).send(res.locals.userCreated);  //redirect to login
+  return res.status(200).redirect('/login');  //redirect to login
 });
 
-app.post('/login', loginControllers.verifyUser, (req, res) => {
-  return res.status(200).send(res.locals.currUser);    //Where do I redirect users when logged in?
-});
+app.post('/login', 
+  loginControllers.verifyUser,
+  loginControllers.createToken, 
+  (req, res) => {
+    console.log('got to the end');
+    return res.status(200).redirect('/afterLogin');    //Where do I redirect users when logged in?
+  });
 
+//temp rout to test token
+app.get('/afterLogin', loginControllers.checkForToken, loginControllers.verifyToken, (req, res) => {
+  res.status(200).send('you can enter');
+});
 
 
 
--- src/server/routes/loginControllers.js
+++ src/server/routes/loginControllers.js
@@ -1,4 +1,5 @@
 const db = require('../models/jobSearchModels');
+const jwt = require('jsonwebtoken');
 
 const loginControllers = {};
 
@@ -7,7 +8,8 @@ const loginControllers = {};
 //OUTPUT: string => new user created
 loginControllers.createUser = (req, res, next) => {
   const { name, password } = req.body;
-  console.log([name, password]);
+
+  //check that all fields are not empty
   if(!name || !password) return next({
     log: 'Express Error handler caught in createUser err',
     status: 500,
@@ -39,15 +41,15 @@ loginControllers.createUser = (req, res, next) => {
 loginControllers.verifyUser = (req, res, next) => {
   console.log('entered');
   const { name, password } = req.body;
-  console.log(name, password);
   const sqlVerify = 'SELECT name, password FROM users WHERE name = $1';
   db.query(sqlVerify, [name])
     .then((result) => {
-      console.log(result.rows[0].password === password);
+      if(password !== result.rows[0].password) throw Error({message: { err: 'wrong password/username' }})
       res.locals.currUser = result.rows[0];
       return next(); 
     })
     .catch(err => {
+      console.log('entered errror')
       return next({
         log: 'Express Error handler caught in verifyUser err',
         status: 500,
@@ -56,9 +58,51 @@ loginControllers.verifyUser = (req, res, next) => {
     });
 };
 
+
+
+
+loginControllers.createToken = (req, res, next) => {
+  console.log('here is the header', req.header);
+  const { name } = req.body;
+  jwt.sign({name}, process.env.JWT_SECRET, { expiresIn: '72h'}, (err, token) => {
+    res.locals.myToken = {token};
+    next();
+  });
+};
+
+
+loginControllers.checkForToken = (req, res, next) => {
+  const header = req.headers['authorization'];
+  if(typeof header !== 'undefined') {
+    const bearer = header.split(' ');
+    const token = bearer[1];
+    req.token = token;
+    next();
+  } else {
+    //If header is undefined return Forbidden (403)
+    res.sendStatus(403);
+  }
+};
+
+loginControllers.verifyToken = (req, res, next) => {
+  //verify the JWT token generated for the user
+  jwt.verify(req.token, process.env.JWT_SECRET, (err, authorizedData) => {
+    if(err){
+      //If error send Forbidden (403)
+      console.log('ERROR: Could not connect to the protected route');
+      res.sendStatus(403);
+    } else {
+      //If token is successfully verified, we can send the autorized data 
+      res.json({
+        message: 'Successful log in',
+        authorizedData
+      });
+      console.log('SUCCESS: Connected to protected route');
+    }
+  });
+};
+
 module.exports = loginControllers;
 
 
 
-//INSERT INTO users (name, password)
-//VALUES ('miketyson', 'boxer');
