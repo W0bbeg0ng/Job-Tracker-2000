1b7b6e56c0cc1e03eda805b4908620dc78c2ff5b
Daniel Steinbrook <danielsteinbrook@Daniels-MacBook-Pro.local>
2022-08-15 14:43:05 -0400

    jobListTest

--- src/client/components/Container.jsx
+++ src/client/components/Container.jsx
@@ -3,6 +3,31 @@ import TabsContainer from "./TabsContainer";
 import JobList from "./JobList";
 import CompanyList from "./CompanyList"
 
+const test = {testList: [{
+  jobTitle: "Sr. Software Engineer",
+  companyName: "Google",
+  jobListingUrl: "http://www.google.com",
+  dateLastChecked: "08-08-08",
+  starred: false,
+  status: "applied",
+  starOnClick: "func",
+  notesText: "okay job",
+  notesSave: "func"
+
+},
+{
+  jobTitle: "Jr. Software Engineer",
+  companyName: "GoogleJr",
+  jobListingUrl: "http://www.google.com",
+  dateLastChecked: "08-08-09",
+  starred: false,
+  status: "interviewing",
+  starOnClick: "func",
+  notesText: "",
+  notesSave: "func"
+
+}]}
+
 const Container = (props) => {
   return (
    <div>
@@ -10,7 +35,7 @@ const Container = (props) => {
       
      <TabsContainer>
        <div label = "Jobs">
-        <JobList />
+        <JobList testList = {test.testList}/>
        </div>
        <div label = "Companies">
         <CompanyList />
--- src/client/components/JobList.jsx
+++ src/client/components/JobList.jsx
@@ -1,32 +1,34 @@
 import React from 'react';
 import JobDisplay from './JobDisplay'
 
-const test = {testList: [{
-    jobTitle: "Sr. Software Engineer",
-    companyName: "Google",
-    jobListingUrl: "http://www.google.com",
-    dateLastChecked: "08-08-08",
-    starred: false,
-    status: "applied",
-    starOnClick: "func",
-    notesText: "okay job"
+// const test = {testList: [{
+//     jobTitle: "Sr. Software Engineer",
+//     companyName: "Google",
+//     jobListingUrl: "http://www.google.com",
+//     dateLastChecked: "08-08-08",
+//     starred: false,
+//     status: "applied",
+//     starOnClick: "func",
+//     notesText: "okay job",
+//     notesSave: "func"
 
-},
-{
-    jobTitle: "Jr. Software Engineer",
-    companyName: "GoogleJr",
-    jobListingUrl: "http://www.google.com",
-    dateLastChecked: "08-08-09",
-    starred: false,
-    status: "interviewing",
-    starOnClick: "func",
-    notesText: ""
+// },
+// {
+//     jobTitle: "Jr. Software Engineer",
+//     companyName: "GoogleJr",
+//     jobListingUrl: "http://www.google.com",
+//     dateLastChecked: "08-08-09",
+//     starred: false,
+//     status: "interviewing",
+//     starOnClick: "func",
+//     notesText: "",
+//     notesSave: "func"
 
-}]}
+// }]}
 
 
 const JobList = (props) => {
-    const jobs = test.testList;
+    const jobs = props.testList;
     const array = jobs.map(element => <JobDisplay jobTitle = {element.jobTitle} companyName= {element.companyName}
         jobListingUrl = {element.jobListingUrl} dateLastChecked = {element.dateLastChecked} starred= {element.starred}
         status = {element.status} starOnClick={element.starOnClick} notesText = {element.notesText}/>)
